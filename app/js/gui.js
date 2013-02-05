00bf020a294ac442e3ef29de8be47484332a4c62
Jason Huggins <jason@jrandolph.com>
2013-02-05 05:41:27 -0600

    Basic menu item toggling.

--- app/js/gui.js
+++ app/js/gui.js
@@ -3,8 +3,24 @@ var gui = require('nw.gui');
 // Setup the tray menu
 var tray = new gui.Tray({icon: 'img/appium_logo_small.png' });
 var menu = new gui.Menu();
-menu.append(new gui.MenuItem({ label: 'Appium: On', enabled : false }));
-menu.append(new gui.MenuItem({ label: 'Turn Appium Off' }));
+var appium_status = new gui.MenuItem({ label: 'Appium: On', enabled : false });
+var appium_toggle = new gui.MenuItem({ label: 'Turn Appium Off'});
+appium_toggle.click = function() { 
+    if (this.label == "Turn Appium Off"){
+        this.label = "Turn Appium On";
+        appium_status.label = "Appium: Off";
+    } else {
+        this.label = "Turn Appium Off";
+        appium_status.label = "Appium: On";
+    } 
+}
+
+menu.append(appium_status);
+menu.append(appium_toggle);
 menu.append(new gui.MenuItem({ label: 'Separator', type : 'separator' }));
 menu.append(new gui.MenuItem({ label: 'Listening on port 4723', enabled : false }));
 tray.menu = menu;
+
+//tray.on('click', function(){
+//   alert('yo');
+//}
