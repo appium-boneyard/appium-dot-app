8b3921db9897ba4b4cfba137e2caa407198eb15f
Dan Cuellar <penguinho@aol.com>
2013-03-06 17:05:21 -0800

    More AppleScript Support

---
+++ obj-c/Appium/Appium/AppiumAppPathTransformer.h
@@ -0,0 +1,13 @@
+//
+//  AppiumAppPathTransformer.h
+//  Appium
+//
+//  Created by Dan Cuellar on 3/6/13.
+//  Copyright (c) 2013 Appium. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppiumAppPathTransformer : NSValueTransformer
+
+@end
