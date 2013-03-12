4ba2e784ae1519635fa120805df212855382ff2d
Dan Cuellar <penguinho@aol.com>
2013-03-12 14:57:25 -0700

    Adding Model Layer to Appium.app

---
+++ obj-c/Appium/Appium/AppiumModel.m
@@ -0,0 +1,13 @@
+//
+//  AppiumModel.m
+//  Appium
+//
+//  Created by Dan Cuellar on 3/12/13.
+//  Copyright (c) 2013 Appium. All rights reserved.
+//
+
+#import "AppiumModel.h"
+
+@implementation AppiumModel
+
+@end
---
+++ obj-c/Appium/Appium/AppiumPortTransformer.m
@@ -0,0 +1,13 @@
+//
+//  AppiumPortTransformer.m
+//  Appium
+//
+//  Created by Dan Cuellar on 3/12/13.
+//  Copyright (c) 2013 Appium. All rights reserved.
+//
+
+#import "AppiumPortTransformer.h"
+
+@implementation AppiumPortTransformer
+
+@end
