df941a061d9f3d35cb76b6fc9bbb21fb0c0f5d7a
Dan Cuellar <penguinho@aol.com>
2013-03-16 11:09:18 -0700

    Removing Python script which is no longer needed

--- obj-c/Appium/Appium/page_source.py
+++
@@ -1,6 +0,0 @@
-from sys import argv
-from selenium import webdriver
-
-driver = webdriver.Remote(command_executor='http://' + argv[1] + ':' + argv[2] + '/wd/hub',desired_capabilities={'browserName': 'iOS','platform': 'Mac', 'version': '6.0'})
-print driver.page_source
-driver.quit()
