2203cb0f81caead6518242ef1291fa93f878bba7
Dan Cuellar <penguinho@aol.com>
2013-04-30 14:38:12 -0700

    Adding more Settings & Preferences

--- obj-c/Appium/Appium/Server/Model/AppiumModel.h
+++ obj-c/Appium/Appium/Server/Model/AppiumModel.h
@@ -20,12 +20,20 @@ typedef enum iOSAutomationDeviceTypes
 	iOSAutomationDevice_iPad
 } iOSAutomationDevice;
 
+typedef enum iOSOrientationTypes
+{
+	iOSOrientation_Portrait,
+	iOSOrientation_Landscape
+} iOSOrientation;
+
 @interface AppiumModel : NSObject
 
 @property NSTask *serverTask;
 
 @property NSString *androidActivity;
+@property NSNumber *androidDeviceReadyTimeout;
 @property NSString *androidPackage;
+@property NSString *androidWaitActivity;
 @property NSString *appPath;
 @property NSString *bundleID;
 @property BOOL checkForUpdates;
@@ -34,19 +42,25 @@ typedef enum iOSAutomationDeviceTypes
 @property BOOL developerMode;
 @property iOSAutomationDevice deviceToForce;
 @property NSString *deviceToForceString;
+@property BOOL fastReset;
 @property BOOL forceDevice;
+@property BOOL forceOrientation;
 @property BOOL isServerRunning;
 @property BOOL isServerListening;
 @property NSString *ipAddress;
 @property BOOL keepArtifacts;
 @property BOOL logVerbose;
+@property iOSOrientation orientationToForce;
+@property NSString* orientationToForceString;
 @property Platform platform;
 @property NSNumber *port;
 @property BOOL prelaunchApp;
 @property BOOL resetApplicationState;
 @property NSString *udid;
 @property BOOL useAndroidActivity;
+@property BOOL useAndroidDeviceReadyTimeout;
 @property BOOL useAndroidPackage;
+@property BOOL useAndroidWaitActivity;
 @property BOOL useAppPath;
 @property BOOL useBundleID;
 @property BOOL useExternalAppiumPackage;
