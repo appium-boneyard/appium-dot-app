212badf7973b10a09ef5a8066aef26edccefb67b
Dan Cuellar <penguinho@aol.com>
2013-10-27 16:01:51 -0700

    Update NodeJS to 0.10.21

--- Appium/AppiumGlobals.h
+++ Appium/AppiumGlobals.h
@@ -8,4 +8,4 @@
 
 #define APPIUM_DEBUG_LEVEL 0
 #define APPIUM_ABSTRACT_CLASS_ERROR @throw [NSException exceptionWithName:NSInternalInconsistencyException reason:[NSString stringWithFormat:@"You must override %@ in a subclass", NSStringFromSelector(_cmd)] userInfo:nil];
-#define NODE_JS_DOWNLOAD_URL @"http://nodejs.org/dist/v0.10.20/node-v0.10.20-darwin-x64.tar.gz"
+#define NODE_JS_DOWNLOAD_URL @"http://nodejs.org/dist/v0.10.21/node-v0.10.21-darwin-x64.tar.gz"
