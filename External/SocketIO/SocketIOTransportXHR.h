9b3a8b04ed38d3b781ce3af261f7bbee9501dad4
Dan Cuellar <penguinho@aol.com>
2013-11-22 13:46:58 -0800

    Updating selenium-objective c

---
+++ External/SocketIO/SocketIOTransportXHR.h
@@ -0,0 +1,39 @@
+//
+//  SocketIOTransportXHR.h
+//  v0.4.1 ARC
+//
+//  based on
+//  socketio-cocoa https://github.com/fpotter/socketio-cocoa
+//  by Fred Potter <fpotter@pieceable.com>
+//
+//  using
+//  https://github.com/square/SocketRocket
+//  https://github.com/stig/json-framework/
+//
+//  reusing some parts of
+//  /socket.io/socket.io.js
+//
+//  Created by Philipp Kyeck http://beta-interactive.de
+//
+//  Updated by
+//    samlown   https://github.com/samlown
+//    kayleg    https://github.com/kayleg
+//    taiyangc  https://github.com/taiyangc
+//
+
+#import <Foundation/Foundation.h>
+
+#import "SocketIOTransport.h"
+
+@interface SocketIOTransportXHR : NSObject <SocketIOTransport, NSURLConnectionDelegate>
+{
+    NSString *_url;
+    NSMutableData *_data;
+    NSMutableDictionary *_polls;
+    BOOL _isClosed;
+}
+
+@property (nonatomic, weak) id <SocketIOTransportDelegate> delegate;
+@property (nonatomic) BOOL isClosed;
+
+@end
