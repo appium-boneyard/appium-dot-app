88027a6bb55824eb24beab2ba10d127764277204
Jamie Edge <edgy360@users.noreply.github.com>
2014-05-23 21:46:54 +0100

    Fixes #229 (Add support for node.js/wd in the Exporter)

---
+++ Appium/Inspector/Recorder/CodeMaker/Plugins/AppiumCodeMakerNodePlugin.h
@@ -0,0 +1,18 @@
+//
+//  AppiumCodeMakerNodePlugin.h
+//  Appium
+//
+//  Created by Jamie Edge on 09/04/2014.
+//  Copyright (c) 2014 Appium. All rights reserved.
+//
+
+#import "AppiumCodeMaker.h"
+
+@class AppiumCodeMaker;
+@class AppiumCodeMakerPlugin;
+
+@interface AppiumCodeMakerNodePlugin : AppiumCodeMakerPlugin
+
+@property (weak) AppiumCodeMaker *codeMaker;
+
+@end
